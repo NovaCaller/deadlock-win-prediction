use proper logging, fixed timestamp normalization messing with joins

prepare_data.py
@@ -1,13 +1,23 @@
+import logging
+
+from src.set_up_logging import set_up_logging
+
+
+
+
 from datetime import datetime
 from pathlib import Path
 from zoneinfo import ZoneInfo
 
 from src.encode_features import encode_features
 from src.filter_matches import filter_matches
-from src.normalize_features import normalize_features
+from src.normalize_non_key_features import normalize_non_key_features
 from src.replace_hero_ids_with_names import replace_hero_ids_with_names
 from src.split_off_timestamps import split_off_timestamps
-from src.write_tensors import write_tensors
+from src.join_dataframes import join_dataframes
+from src.util import normalize_df
+from src.pytorch_setup import ensure_torch
+
 
 # start_time between patches (2025-10-02T22:03:05+0200 to 2025-10-25T01:54:51+0200 (+1h on start and -1h on end)
 # is_high_skill_range_parties: false
@@ -31,20 +41,27 @@ END_DATETIME: datetime = datetime(2025, 11, 21, 22, 53, 12, tzinfo=ZoneInfo("Eur
 MIN_RANK_BADGE: int = 101
 MAX_RANK_DISPARITY: int = 2
 LEAVER_TIME_TO_LEAVE_BEFORE_MATCH_END_LENIENCY: int = 70 # players can leave 70s before match end to not be considered leavers
+LOG_LEVEL: int = logging.DEBUG
 
 
 if __name__ == "__main__":
+    set_up_logging(LOG_LEVEL)
+
+    ensure_torch()
+    # noinspection PyPackageRequirements,PyUnresolvedReferences
+    import torch
+
     OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
     PROCESSED_PATH.mkdir(parents=True, exist_ok=True)
 
     info_df, player_df = filter_matches(MATCH_METADATA_PATH, RELEVANT_MATCH_ID_RANGE, RELEVANT_MATCH_INFO_COLUMNS, START_DATETIME, END_DATETIME, MIN_RANK_BADGE, MAX_RANK_DISPARITY, RELEVANT_MATCH_PLAYER_COLUMNS, LEAVER_TIME_TO_LEAVE_BEFORE_MATCH_END_LENIENCY)
-    print("done filtering matches.")
+    logging.info("done filtering matches.")
 
     player_df = replace_hero_ids_with_names(player_df, HEROES_PARQUET)
-    print("done replacing hero ids with names.")
+    logging.info("done replacing hero ids with names.")
 
     info_general_df, info_timestamp_df, player_general_df, player_timestamp_df = split_off_timestamps(info_df, player_df)
-    print("done splitting data into general and timestamps.")
+    logging.info(f"done splitting data into general and timestamps.\ninfo timestamps: {info_timestamp_df.shape[0]}\nplayer timestamps: {player_timestamp_df.shape[0]}.")
 
     info_general_df.to_parquet(OUTPUT_PATH / "match_info_general.parquet")
     info_timestamp_df.to_parquet(OUTPUT_PATH / "match_info_timestamp.parquet")
@@ -55,16 +72,20 @@ if __name__ == "__main__":
     player_general_df.drop(["ability_points", "player_level", "net_worth"], axis=1, inplace=True)
 
     info_general_df, info_timestamp_df, player_general_df, player_timestamp_df = encode_features(info_general_df, info_timestamp_df, player_general_df, player_timestamp_df)
-    print("done encoding features.")
+    logging.info("done encoding features.")
 
-    info_general_df, info_timestamp_df, player_general_df, player_timestamp_df = normalize_features(info_general_df, info_timestamp_df, player_general_df, player_timestamp_df)
-    print("done normalizing features.")
-    print("finalized dataframes.")
+    info_general_df, info_timestamp_df, player_general_df, player_timestamp_df = normalize_non_key_features(info_general_df, info_timestamp_df, player_general_df, player_timestamp_df)
+    logging.info("done normalizing features (except timestamps).")
 
-    info_general_df.to_parquet(PROCESSED_PATH / "info_general_final.parquet")
-    info_timestamp_df.to_parquet(PROCESSED_PATH / "info_timestamp_final.parquet")
-    player_general_df.to_parquet(PROCESSED_PATH / "player_general_final.parquet")
-    player_timestamp_df.to_parquet(PROCESSED_PATH / "player_timestamp_final.parquet")
+    merged_df = join_dataframes(info_general_df, info_timestamp_df, player_general_df, player_timestamp_df)
+    logging.info(f"merged dataframes to single dataframe with {merged_df.shape[0]} rows and {merged_df.shape[1]} columns.")
 
-    write_tensors(info_general_df, info_timestamp_df, player_general_df, player_timestamp_df, PROCESSED_PATH / "tensors.pt")
-    print("done writing tensors.")
+    merged_df = normalize_df(merged_df, ["timestamp"])
+    logging.info("done normalizing timestamps.")
+    logging.info("finalized dataframe.")
+
+    tensor = torch.from_numpy(merged_df.values)
+    logging.info(
+        f"converted to tensor with {tensor.shape[0]} rows and {tensor.shape[1]} columns.")
+    torch.save(tensor, PROCESSED_PATH / "tensor.pt")
+    logging.info("wrote tensor to disk.")

src/join_dataframes.py
@@ -1,19 +1,12 @@
-from pathlib import Path
+import logging
 
 import duckdb
 import pandas as pd
 
-from src.pytorch_setup import ensure_torch
-
-ensure_torch()
-# noinspection PyPackageRequirements,PyUnresolvedReferences
-import torch
-
-DEBUG_QUERY = False
 
 # noinspection PyUnusedLocal
-def write_tensors(info_general_df: pd.DataFrame, info_timestamp_df: pd.DataFrame, player_general_df: pd.DataFrame,
-                  player_timestamp_df: pd.DataFrame, output_file_path: Path) -> None:
+def join_dataframes(info_general_df: pd.DataFrame, info_timestamp_df: pd.DataFrame, player_general_df: pd.DataFrame,
+                    player_timestamp_df: pd.DataFrame) -> pd.DataFrame:
     rel = duckdb.sql(f"""
         WITH
             numbered_players AS (
@@ -25,7 +18,7 @@ def write_tensors(info_general_df: pd.DataFrame, info_timestamp_df: pd.DataFrame
                     pt.* EXCLUDE (match_id, account_id, timestamp_s)
                 FROM player_timestamp_df as pt
                 JOIN player_general_df as pg
-                    USING ("match_id", "account_id")
+                    USING (match_id, account_id)
             )
         SELECT
             * EXCLUDE (match_id, winning_team, timestamp_s, team, player_num),
@@ -48,14 +41,6 @@ def write_tensors(info_general_df: pd.DataFrame, info_timestamp_df: pd.DataFrame
         ;
     """)
 
-    # --- debugging ---
-    if DEBUG_QUERY:
-        df = rel.fetchdf()
-        print(df.describe())
-        print(df.info())
-        print(df.head())
-    # --- end of debugging ---
-
-    result = rel.torch()
-    as_single_tensor = torch.stack([result[col] for col in result.keys()], dim=1)
-    torch.save(as_single_tensor, output_file_path)
+    df = rel.fetchdf()
+    logging.debug(f"number of missing values in merged df: {df.isna().sum().sum()}")
+    return df